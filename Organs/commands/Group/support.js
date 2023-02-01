a509a947ad62809fa70ddbcdccbd67c8d92bfc2a
||•carter•|| <98878721+dark-worf@users.noreply.github.com>
2023-02-01 15:06:31 +0200

    Update support.js

--- Organs/commands/Group/support.js
+++ Organs/commands/Group/support.js
@@ -21,13 +21,13 @@ const lemo = `
 *━『 Support Group Links 』━*
 
 *1) [ Ɇ𝖙𝖊𝖗𝖓𝖎𝖙ɏ ] :*
-https://chat.whatsapp.com/JCCZPbPUbM1536n62zSFZi
+https://chat.whatsapp.com/KcjlyBak14VD3lSMcyIVzI
 
 *2) [ Ɇ𝖙𝖊𝖗𝖓𝖎𝖙ɏ ₵𝖆𝖘𝖎𝖓ø ] 🎰 :*
-https://chat.whatsapp.com/J1WfFPXtmzZGXFB59I0ETq
+https://chat.whatsapp.com/
 
 *3) [ Ɇ𝖙𝖊𝖗𝖓𝖎𝖙ɏ N̸s̸f̸w̸ ] :*
-https://chat.whatsapp.com/J1WfFPXtmzZGXFB59I0ETq
+https://chat.whatsapp.com/
 `
   await client.sendMessage(m.from,{video:{url:rae}, gifPlayback:true, caption: `*${pushName}* have a look in your DM`},{quoted:m})
   await  client.sendMessage(m.sender,{image:{url:eternitylogogg},caption:lemo},{quoted:m})  
